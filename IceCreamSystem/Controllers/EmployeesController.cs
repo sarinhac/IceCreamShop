7305ccf0852922e3b245ca209ff27b70831228b3
sarinhac <sarahangelica.c@gmail.com>
2020-11-08 20:38:25 -0300

    Create Employee

--- IceCreamSystem/Controllers/EmployeesController.cs
+++ IceCreamSystem/Controllers/EmployeesController.cs
@@ -21,6 +21,7 @@ namespace IceCreamSystem.Controllers
             return View(employee.ToList());
         }
 
+        #region Actions Login
         public ActionResult Home()
         {
             return View();
@@ -54,9 +55,11 @@ namespace IceCreamSystem.Controllers
             ViewBag.error = "Login or Password Invalid, Please try again";
             return View();
         }
-
+        #endregion
 
         #region CRUD
+
+        #region Actions Create 
         public ActionResult Create()
         {
             IEnumerable<SelectListItem> permission = new SelectList(Enum.GetValues(typeof(Permission)));
@@ -80,57 +83,108 @@ namespace IceCreamSystem.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Employee employee)
+        public ActionResult Create([Bind(Include = "NameEmployee, Birth, Admission, Salary, OfficeId, CompanyId, HaveLogin, Permission, LoginUser, PasswordUser")] Employee employee,
+            [Bind(Include = "Cep, Logradouro, Numero, Bairro, Cidade, Uf")] Address address)
         {
-            if (ModelState.IsValid)
+            #region CATCHING ALL PHONES IN REQUEST
+            List<Phone> phones = new List<Phone>();
+            List<string> request = Request.Form.ToString().Split('&').Where(p => p.Contains("DDD") || p.Contains("TypePhone") || p.Contains("Number")).ToList();
+
+            for (int i = 0; i < request.Count; i++)
             {
-                #region CATCHING ALL PHONES IN REQUEST
-                List<Phone> phones = new List<Phone>();
-                List<string> request = Request.Form.ToString().Split('&').Where(p => p.Contains("DDD") || p.Contains("TypePhone") || p.Contains("Number")).ToList();
+                Phone phoneRequest = new Phone();
 
-                for (int i = 0; i < request.Count; i++)
+                if (request[i].Contains("TypePhone"))
                 {
-                    Phone phoneRequest = new Phone();
+                    phoneRequest.TypePhone = request[i].Replace("TypePhone=", "").ToString().Equals("Mobile") ? (TypePhone)1 : (TypePhone)2; //1 => Mobile | 2 => Landline
 
-                    if (request[i].Contains("TypePhone"))
-                    {
-                        phoneRequest.TypePhone = request[i].Replace("TypePhone=", "").ToString().Equals("Mobile") ? (TypePhone)1 : (TypePhone)2; //1 => Mobile | 2 => Landline
+                    int index = request.IndexOf(request.Where(p => p.ToString().Contains("DDD=")).FirstOrDefault());
+                    phoneRequest.DDD = request[index].Replace("DDD=", "");
+                    request[index] = request[index].Replace("DDD=", "");
 
-                        int index = request.IndexOf(request.Where(p => p.ToString().Contains("DDD=")).FirstOrDefault());
-                        phoneRequest.DDD = request[index].Replace("DDD=", "");
-                        request[index] = request[index].Replace("DDD=", "");
+                    index = request.IndexOf(request.Where(p => p.ToString().Contains("Number=")).FirstOrDefault());
+                    phoneRequest.Number = request[index].Replace("Number=", "");
+                    request[index] = request[index].Replace("Number=", "");
 
-                        index = request.IndexOf(request.Where(p => p.ToString().Contains("Number=")).FirstOrDefault());
-                        phoneRequest.Number = request[index].Replace("Number=", "");
-                        request[index] = request[index].Replace("Number=", "");
-
-                        phones.Add(phoneRequest);
-
-                    }
-                    else
-                        break;
+                    phones.Add(phoneRequest);
 
                 }
-                #endregion
+                else
+                    break;
 
-                if (employee.HaveLogin)
-                    employee.PasswordUser = HashService.HashPassword(employee.PasswordUser);
+            }
+            #endregion
 
-                db.Employee.Add(employee);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                var currentUser = db.Employee.SingleOrDefault(
+                u => u.LoginUser.Equals(employee.LoginUser));
+
+                if (currentUser == null && employee.LoginUser != null)
+                {
+                    using (var trans = db.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            #region SAVE NEW EMPLOYEE
+                            int idUser = (int)Session["idUser"]; //who is login
+                            if (employee.HaveLogin)
+                                employee.PasswordUser = HashService.HashPassword(employee.PasswordUser);
+
+                            db.Address.Add(address);
+                            db.SaveChanges();
+
+                            employee.AddressId = address.IdAddress;
+                            db.Employee.Add(employee);
+                            db.SaveChanges();
+
+                            foreach (Phone p in phones)
+                                p.EmployeeId = employee.IdEmployee;
+
+                            db.Phone.AddRange(phones);
+                            db.SaveChanges();
+
+                            Log log = new Log
+                            {
+                                New = employee.NameEmployee + "/BD " + employee.Birth.ToString("dd/MM/yy") + "/AD " + employee.Admission.ToString("dd/MM/yy") + "/ " + employee.Salary
+                                + "/Office " + employee.OfficeId + "/Address " + address.Cep + "/ " + address.Numero
+                                + "/ " + address.Cidade + "/ " + address.Uf + "/ " + employee.LoginUser != null ? employee.LoginUser : "",
+
+                                Who = idUser,
+                                EmployeeId = employee.IdEmployee
+                            };
+                            db.Log.Add(log);
+                            db.SaveChanges();
+
+                            trans.Commit();
+                            return RedirectToAction("Index");
+                            #endregion
+                        }
+                        catch
+                        {
+                            trans.Rollback();
+                            goto ReturnIfError;
+                        }
+                    }
+                }
+                goto ReturnIfError;
             }
 
+        ReturnIfError:
             IEnumerable<SelectListItem> permission = new SelectList(Enum.GetValues(typeof(Permission)));
             ViewBag.Permission = permission;
 
-            IEnumerable<SelectListItem> phone = new SelectList(Enum.GetValues(typeof(TypePhone)));
-            ViewBag.TypePhone = phone;
+            ViewBag.Phones = phones;
+
+            IEnumerable<SelectListItem> typePhone = new SelectList(Enum.GetValues(typeof(TypePhone)));
+            ViewBag.TypePhone = typePhone;
 
             ViewBag.CompanyId = new SelectList(db.Company, "IdCompany", "NameCompany", employee.CompanyId);
             ViewBag.OfficeId = new SelectList(db.Office, "IdOffice", "NameOffice", employee.OfficeId);
             return View(employee);
         }
+
+        #endregion
 
         public ActionResult Details(int? id)
         {
@@ -146,20 +200,29 @@ namespace IceCreamSystem.Controllers
             return View(employee);
         }
 
+        #region Actions Edit
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Employee employee = db.Employee.Find(id);
             if (employee == null)
             {
                 return HttpNotFound();
             }
 
+            List<Phone> phones = db.Phone.Where(p => p.EmployeeId == id).ToList();
+            ViewBag.Phones = phones;
+
             IEnumerable<SelectListItem> permission = new SelectList(Enum.GetValues(typeof(Permission)));
             ViewBag.Permission = permission;
+
+            IEnumerable<SelectListItem> typePhone = new SelectList(Enum.GetValues(typeof(TypePhone)));
+            ViewBag.TypePhone = typePhone;
+           
             ViewBag.CompanyId = new SelectList(db.Company, "IdCompany", "NameCompany", employee.CompanyId);
             ViewBag.OfficeId = new SelectList(db.Office, "IdOffice", "NameOffice", employee.OfficeId);
             return View(employee);
@@ -182,6 +245,7 @@ namespace IceCreamSystem.Controllers
             ViewBag.OfficeId = new SelectList(db.Office, "IdOffice", "NameOffice", employee.OfficeId);
             return View(employee);
         }
+        #endregion
 
         public ActionResult Delete(int? id)
         {
